744d8918f37a0bfd5504fb762573275fff9142f2
Imtiajul <iimtiajul@gmail.com>
2022-12-03 18:53:37 +0600

    Individual author page added

---
+++ src/pages/SingleAuthor.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom';
+import client from '../client'
+
+function SingleAuthor() {
+   const [posts, setPosts] = useState([])
+   const [isloading, setIsLoading] = useState(true)
+   const { slug } = useParams()
+
+   useEffect(() => {
+      client.fetch(
+         `*[_type == "post" && 
+         author->slug.current == "${slug}"] {
+            title, 
+            slug, 
+            body,
+            mainImage {
+              asset -> {
+                _id,
+                url
+              },
+              alt
+            }
+         }`
+      ).then((data) => { setPosts(data) })
+         .catch(console.error);
+      setIsLoading(false)
+   }, [])
+   return (
+      <>
+         {isloading ? <h1 className="text-center text-5xl font-bold mt-5 mb-10 tracking-widest md:text-6xl lg:text-7xl flex h-screen justify-center items-center">Loading...</h1> :
+            <section className="p-5 2xl:max-w-7xl 2xl:mx-auto">
+               <h1 className="text-center text-5xl font-bold mt-5 mb-10 tracking-widest md:text-6xl lg:text-7xl">Blog Page</h1>
+                  <div className="grid gap-5 grid-col-1 md:grid-cols-2 lg:grid-cols-3">
+                     {posts.map((post) => (
+                        <article className="mb-5" key={post.slug.current}>
+                           <div className="xl:h-4/6 md:h-3/6 overflow-hidden">
+                              <img src={post.mainImage.asset.url} alt={post.title} />
+                           </div>
+                           <h2 className="text-2xl my-4 mb-5 font-medium">{post.title}</h2>
+                           <Link to={`/blog/${post.slug.current}`} className="py-2 px-6 bg-gray-600 rounded shadow text-white hover:bg-transparent-border-2 border-black transition-all-duration-500 hover:text-black font-bold hover:bg-gray-300 hover:border-gray-900 transition-colors duration-400 border-1">Read Full Article</Link>
+                        </article>
+                     ))}
+                  </div>
+        </section>
+         }
+      </>
+   )
+}
+
+export default SingleAuthor
