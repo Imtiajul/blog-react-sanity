7475d7fb38e3448d7341d52a863f4f9c2cd23e3f
Imtiajul <iimtiajul@gmail.com>
2022-12-03 21:29:37 +0600

    added author name on the heading

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import client from '../client'
 
@@ -6,8 +6,8 @@ function Blog() {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
-   client.fetch(
-    `*[_type == "post"] {
+    client.fetch(
+      `*[_type == "post"] {
       title, 
       slug, 
       body,
@@ -19,26 +19,26 @@ function Blog() {
         alt
       }
     }`
-   ).then((data) => {setPosts(data)})
-   .catch(console.error);
+    ).then((data) => { setPosts(data) })
+      .catch(console.error);
   }, [])
   return (
     <>
-    <section className="p-5 2xl:max-w-7xl 2xl:mx-auto">
-      <h1 className="text-center text-5xl font-bold mt-5 mb-10 tracking-widest md:text-6xl lg:text-7xl">Blog Page</h1>
-     
-      <div className="grid gap-5 grid-col-1 md:grid-cols-2 lg:grid-cols-3">
-        {posts.map((post) => (
-          <article className="mb-5" key={post.slug.current}>
-            <div className="xl:h-4/6 md:h-3/6 overflow-hidden">
-            <img src={post.mainImage.asset.url} alt={post.title} />
-            </div>
-            <h2 className="text-2xl my-4 mb-5 font-medium">{post.title}</h2>
-            <Link to={`/blog/${post.slug.current}`} className="py-2 px-6 bg-gray-600 rounded shadow text-white hover:bg-transparent-border-2 border-black transition-all-duration-500 hover:text-black font-bold hover:bg-gray-300 hover:border-gray-900 transition-colors duration-400 border-1">Read Full Article</Link>
-          </article>
-        ))}
-      </div>
-    </section>
+      <section className="p-5 2xl:max-w-7xl 2xl:mx-auto">
+        <h1 className="text-center text-5xl font-bold mt-5 mb-10 tracking-widest md:text-6xl lg:text-7xl">Blog Page</h1>
+
+        <div className="grid gap-5 grid-col-1 md:grid-cols-2 lg:grid-cols-3">
+          {posts.map((post) => (
+            <article className="mb-5" key={post.slug.current}>
+              <div className="xl:h-4/6 md:h-3/6 overflow-hidden">
+                <Link to={`/blog/${post.slug.current}`}><img src={post.mainImage.asset.url} alt={post.title} /></Link>
+              </div>
+              <h2 className="text-2xl my-4 mb-5 font-medium"><Link to={`/blog/${post.slug.current}`}>{post.title}</Link></h2>
+              <Link to={`/blog/${post.slug.current}`} className="py-2 px-6 bg-gray-600 rounded shadow text-white hover:bg-transparent-border-2 border-black transition-all-duration-500 hover:text-black font-bold hover:bg-gray-300 hover:border-gray-900 transition-colors duration-400 border-1">Read Full Article</Link>
+            </article>
+          ))}
+        </div>
+      </section>
     </>
   )
 }
--- src/pages/SingleAuthor.jsx
+++ src/pages/SingleAuthor.jsx
@@ -14,6 +14,7 @@ function SingleAuthor() {
             title, 
             slug, 
             body,
+            author -> {name},
             mainImage {
               asset -> {
                 _id,
@@ -30,19 +31,19 @@ function SingleAuthor() {
       <>
          {isloading ? <h1 className="text-center text-5xl font-bold mt-5 mb-10 tracking-widest md:text-6xl lg:text-7xl flex h-screen justify-center items-center">Loading...</h1> :
             <section className="p-5 2xl:max-w-7xl 2xl:mx-auto">
-               <h1 className="text-center text-5xl font-bold mt-5 mb-10 tracking-widest md:text-6xl lg:text-7xl">Blog Page</h1>
-                  <div className="grid gap-5 grid-col-1 md:grid-cols-2 lg:grid-cols-3">
-                     {posts.map((post) => (
-                        <article className="mb-5" key={post.slug.current}>
-                           <div className="xl:h-4/6 md:h-3/6 overflow-hidden">
-                              <img src={post.mainImage.asset.url} alt={post.title} />
-                           </div>
-                           <h2 className="text-2xl my-4 mb-5 font-medium">{post.title}</h2>
-                           <Link to={`/blog/${post.slug.current}`} className="py-2 px-6 bg-gray-600 rounded shadow text-white hover:bg-transparent-border-2 border-black transition-all-duration-500 hover:text-black font-bold hover:bg-gray-300 hover:border-gray-900 transition-colors duration-400 border-1">Read Full Article</Link>
-                        </article>
-                     ))}
-                  </div>
-        </section>
+               <h1 className=" text-5xl font-bold mt-5 mb-10 tracking-widest md:text-6xl">Author {posts[0] && posts[0].author && posts[0].author.name}</h1>
+               <div className="grid gap-5 grid-col-1 md:grid-cols-2 lg:grid-cols-3">
+                  {posts.map((post) => (
+                     <article className="mb-5" key={post.slug.current}>
+                        <div className="xl:h-4/6 md:h-3/6 overflow-hidden">
+                           <Link to={`/blog/${post.slug.current}`}><img src={post.mainImage.asset.url} alt={post.title} /></Link>
+                        </div>
+                        <h2 className="text-2xl my-4 mb-5 font-medium"><Link to={`/blog/${post.slug.current}`}>{post.title}</Link></h2>
+                        <Link to={`/blog/${post.slug.current}`} className="py-2 px-6 bg-gray-600 rounded shadow text-white hover:bg-transparent-border-2 border-black transition-all-duration-500 hover:text-black font-bold hover:bg-gray-300 hover:border-gray-900 transition-colors duration-400 border-1">Read Full Article</Link>
+                     </article>
+                  ))}
+               </div>
+            </section>
          }
       </>
    )
